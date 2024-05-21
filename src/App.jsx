4d01d6db06cb5375823eae14de4eb628b7c3b6db
Prajwol Gautam <prajwolgautam@pg-2.local>
2024-05-21 12:01:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Happiness from './Components/Posters/Happiness.jsx'
+
+function App() {
+
+  return (
+   
+      <div>
+        <Happiness />
+      </div>
+        
+      
+     
+  )
+}
+
+export default App
